b9fed8b5382efd5ab9dd6a7deafd120fc847e6e4
JesusRiosLop <jriolop218@g.educaand.es>
2026-01-16 20:37:14 +0100

    comienzo despliegue

---
+++ despliegue/variable.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type = string
+    default = "us-east-1"
+}
+
+variable "domain" {
+    type = string
+    default = "J-J-PROYECT.com"
+}
+
+variable "key_name" {
+    type = string
+    default = "vockey"
+}
