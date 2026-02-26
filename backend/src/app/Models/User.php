4c4004095458d3802b6340be754235d72aa6ba29
jesusrios <jriolop218@g.educaand.es>
2026-02-26 17:00:20 +0100

    pequeños cambios

--- backend/src/app/Models/User.php
+++ backend/src/app/Models/User.php
@@ -6,15 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens; // <--- 1. AÑADIR ESTA LÍNEA (Importar)
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-
-    // 2. AÑADIR 'HasApiTokens' DENTRO DE LA CLASE
-    // Debe estar junto a HasFactory y Notifiable, separados por comas.
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
