d8c5556df6ab8e7ed4776c745a5846dcf78961ea
JaimeDVP562 <jgavtor562@g.educaand.es>
2026-03-28 11:13:03 +0100

    Ignore Terraform local files and untrack .terraform and tfstate

--- despliegue/main.tf
+++ despliegue/main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.28.0"
     }
   }
@@ -18,128 +18,120 @@ provider "aws" {
 #GRUPOS
 
 resource "aws_security_group" "bastion" {
-    name = "bastion"
-    description = "Grupo de seguridad para bastion"
+  name        = "bastion"
+  description = "Grupo de seguridad para bastion"
 }
 
 resource "aws_security_group" "frontend" {
-    name = "frontend"
-    description = "Grupo de seguridad para frontend"
+  name        = "frontend"
+  description = "Grupo de seguridad para frontend"
 }
 
 resource "aws_security_group" "api" {
-    name = "api"
-    description = "Grupo de seguridad para api"
+  name        = "api"
+  description = "Grupo de seguridad para api"
 }
 
 resource "aws_security_group" "database" {
-    name        = "database"
-    description = "Grupo de seguridad para base de datos"
+  name        = "database"
+  description = "Grupo de seguridad para base de datos"
 }
 
 
 #REGLAS BASTION
 
 resource "aws_vpc_security_group_ingress_rule" "bastion_ssh" {
-    security_group_id = aws_security_group.bastion.id
-    ip_protocol = "tcp"
-    from_port = 22
-    to_port = 22
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.bastion.id
+  ip_protocol       = "tcp"
+  from_port         = 22
+  to_port           = 22
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "bastion_egress" {
-    security_group_id = aws_security_group.bastion.id
-    ip_protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.bastion.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 #REGLAS FRONTEND
 
 resource "aws_vpc_security_group_ingress_rule" "frontend_ssh" {
-    security_group_id = aws_security_group.frontend.id
-    ip_protocol = "tcp"
-    from_port = 22
-    to_port = 22
-    referenced_security_group_id = aws_security_group.bastion.id
+  security_group_id            = aws_security_group.frontend.id
+  ip_protocol                  = "tcp"
+  from_port                    = 22
+  to_port                      = 22
+  referenced_security_group_id = aws_security_group.bastion.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "frontend_http" {
-    security_group_id = aws_security_group.frontend.id
-    ip_protocol = "tcp"
-    from_port = 80
-    to_port = 80
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.frontend.id
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "frontend_https" {
-    security_group_id = aws_security_group.frontend.id
-    ip_protocol = "tcp"
-    from_port = 443
-    to_port = 443
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.frontend.id
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 resource "aws_vpc_security_group_egress_rule" "frontend_egress" {
-    security_group_id = aws_security_group.frontend.id
-    ip_protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.frontend.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 #REGLAS API
 
 resource "aws_vpc_security_group_ingress_rule" "api_ssh" {
-    security_group_id = aws_security_group.api.id
-    ip_protocol = "tcp"
-    from_port = 22
-    to_port = 22
-    referenced_security_group_id = aws_security_group.bastion.id
+  security_group_id            = aws_security_group.api.id
+  ip_protocol                  = "tcp"
+  from_port                    = 22
+  to_port                      = 22
+  referenced_security_group_id = aws_security_group.bastion.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "api_http" {
-    security_group_id = aws_security_group.api.id
-    ip_protocol = "tcp"
-    from_port = 80
-    to_port = 80
-    referenced_security_group_id = aws_security_group.frontend.id
+  security_group_id            = aws_security_group.api.id
+  ip_protocol                  = "tcp"
+  from_port                    = 80
+  to_port                      = 80
+  referenced_security_group_id = aws_security_group.frontend.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "api_egress" {
-    security_group_id = aws_security_group.api.id
-    ip_protocol = "-1"
-    from_port = 0
-    to_port = 0
-    cidr_ipv4 = "0.0.0.0/0"
+  security_group_id = aws_security_group.api.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 # REGLAS DATABASE
 
 resource "aws_vpc_security_group_ingress_rule" "database_ssh" {
-    security_group_id            = aws_security_group.database.id
-    ip_protocol                  = "tcp"
-    from_port                    = 22
-    to_port                      = 22
-    referenced_security_group_id = aws_security_group.bastion.id
+  security_group_id            = aws_security_group.database.id
+  ip_protocol                  = "tcp"
+  from_port                    = 22
+  to_port                      = 22
+  referenced_security_group_id = aws_security_group.bastion.id
 }
 
 resource "aws_vpc_security_group_ingress_rule" "database_mysql" {
-    security_group_id            = aws_security_group.database.id
-    ip_protocol                  = "tcp"
-    from_port                    = 3306
-    to_port                      = 3306
-    referenced_security_group_id = aws_security_group.api.id
+  security_group_id            = aws_security_group.database.id
+  ip_protocol                  = "tcp"
+  from_port                    = 3306
+  to_port                      = 3306
+  referenced_security_group_id = aws_security_group.api.id
 }
 
 resource "aws_vpc_security_group_egress_rule" "database_egress" {
-    security_group_id = aws_security_group.database.id
-    ip_protocol       = "-1"
-    from_port         = 0
-    to_port           = 0
-    cidr_ipv4         = "0.0.0.0/0"
+  security_group_id = aws_security_group.database.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
 }
 
 #################################
@@ -149,53 +141,53 @@ resource "aws_vpc_security_group_egress_rule" "database_egress" {
 #BASTION
 
 resource "aws_instance" "bastion" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t2.medium"
-    key_name = var.key_name
-    vpc_security_group_ids = [aws_security_group.bastion.id]
-    user_data = templatefile("${path.module}/templates/bastion.sh.tftpl" , {
-        domain = var.domain
-    })
-    tags = {Name = "Bastion"}
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.medium"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.bastion.id]
+  user_data = templatefile("${path.module}/templates/bastion.sh.tftpl", {
+    domain = var.domain
+  })
+  tags = { Name = "Bastion" }
 }
 
 #FRONTEND
 
 resource "aws_instance" "frontend" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t2.large"
-    key_name = var.key_name
-    vpc_security_group_ids = [aws_security_group.frontend.id]
-    user_data = templatefile("${path.module}/templates/frontend.sh.tftpl" , {
-        domain = var.domain
-    })
-    tags = {Name = "Frontend"}
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.large"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.frontend.id]
+  user_data = templatefile("${path.module}/templates/frontend.sh.tftpl", {
+    domain = var.domain
+  })
+  tags = { Name = "Frontend" }
 }
 
 #API
 
 resource "aws_instance" "api" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t2.large"
-    key_name = var.key_name
-    vpc_security_group_ids = [aws_security_group.api.id]
-    user_data = templatefile("${path.module}/templates/api.sh.tftpl" , {
-        domain = var.domain
-    })
-    tags = {Name = "API"}
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.large"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.api.id]
+  user_data = templatefile("${path.module}/templates/api.sh.tftpl", {
+    domain = var.domain
+  })
+  tags = { Name = "API" }
 }
 
 # DATABASE
 
 resource "aws_instance" "database" {
-    ami = data.aws_ami.ubuntu.id
-    instance_type = "t2.medium"
-    key_name = var.key_name
-    vpc_security_group_ids = [aws_security_group.database.id]
-    user_data = templatefile("${path.module}/templates/database.sh.tftpl", {
-        domain = var.domain
-    })
-    tags = {Name = "Database"}
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.medium"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.database.id]
+  user_data = templatefile("${path.module}/templates/database.sh.tftpl", {
+    domain = var.domain
+  })
+  tags = { Name = "Database" }
 }
 
 
@@ -204,9 +196,9 @@ resource "aws_instance" "database" {
 #################################
 
 resource "aws_eip" "bastion" {
-    domain   = "vpc"
-    instance = aws_instance.bastion.id
-    tags     = { Name = "Bastion-EIP" }
+  domain   = "vpc"
+  instance = aws_instance.bastion.id
+  tags     = { Name = "Bastion-EIP" }
 }
 
 #################################
@@ -214,37 +206,37 @@ resource "aws_eip" "bastion" {
 #################################
 
 resource "aws_route53_zone" "zona_privada" {
-    name = var.domain
+  name = var.domain
 }
 
 resource "aws_route53_record" "bastion" {
-    zone_id = aws_route53_zone.zona_privada.zone_id
-    name = "bastion.${var.domain}"
-    type = "A"
-    ttl = 300
-    records = [aws_instance.bastion.private_ip]
+  zone_id = aws_route53_zone.zona_privada.zone_id
+  name    = "bastion.${var.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.bastion.private_ip]
 }
 
 resource "aws_route53_record" "frontend" {
-    zone_id = aws_route53_zone.zona_privada.zone_id
-    name = "frontend.${var.domain}"
-    type = "A"
-    ttl = 300
-    records = [aws_instance.frontend.private_ip]
+  zone_id = aws_route53_zone.zona_privada.zone_id
+  name    = "frontend.${var.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
 }
 
 resource "aws_route53_record" "api" {
-    zone_id = aws_route53_zone.zona_privada.zone_id
-    name = "api.${var.domain}"
-    type = "A"
-    ttl = 300
-    records = [aws_instance.api.private_ip]
+  zone_id = aws_route53_zone.zona_privada.zone_id
+  name    = "api.${var.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.api.private_ip]
 }
 
 resource "aws_route53_record" "database" {
-    zone_id = aws_route53_zone.zona_privada.zone_id
-    name    = "database.${var.domain}"
-    type    = "A"
-    ttl     = 300
-    records = [aws_instance.database.private_ip]
+  zone_id = aws_route53_zone.zona_privada.zone_id
+  name    = "database.${var.domain}"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.database.private_ip]
 }
